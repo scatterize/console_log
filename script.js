059c26dce5778c410a46f6c9daf7c1ac9443b127
scatterize <scatterize>
2020-06-27 10:00:57 +0300

    less15 console

--- script.js
+++ script.js
@@ -10,11 +10,35 @@ const personalMovieDB = {
   privat: false,
 };
 
-youLatestFilm = prompt('Один из последних просмотренных фильмов?', '');
-youFilmRating = prompt('На сколько оцените его?', '');
-let youLatestFilm2 = prompt('Один из последних просмотренных фильмов?', '');
-let youFilmRating3 = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[youLatestFilm] = youFilmRating;
-personalMovieDB.movies[youLatestFilm2] = youFilmRating3;
+//youFilmRating = prompt('На сколько оцените его?', '');
+//let youFilmRating3 = prompt('На сколько оцените его?', '');
+//personalMovieDB.movies[youLatestFilm2] = youFilmRating3;
+// youLatestFilm = prompt('Один из последних просмотренных фильмов?', '');
+// let youLatestFilm2 = prompt('Один из последних просмотренных фильмов?', '');
+// personalMovieDB.movies[youLatestFilm] = youFilmRating;
 
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+  let k = prompt('Один из последних просмотренных фильмов?', '');
+  let j = prompt('На сколько оцените его?', '');
+
+  if (k != null && j != null && k != '' && j != '' && k.length < 50) {
+    personalMovieDB.movies[k] = j;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (ersonalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
+// if (youLatestFilm == '' || youLatestFilm.length > 50) {
+//   alert('incorect value');
+// }
